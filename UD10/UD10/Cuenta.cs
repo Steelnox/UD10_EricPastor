5a7bee40004963dced49d8552efd1a528b4f6c28
Steelnox <eric14pastor@gmail.com>
2021-01-04 10:28:48 +0100

    Ejercicio 1 realizado (Clase cuenta creada)

---
+++ UD10/UD10/Cuenta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UD10
+{
+    public class Cuenta
+    {
+        private string titular;
+        private double cantidad;
+
+        public Cuenta(string titular)
+        {
+            this.titular = titular;
+            cantidad = 0;
+        }
+
+        public Cuenta(string titular, double cantidad)
+        {
+            this.titular = titular;
+            this.cantidad = cantidad;
+        }
+
+        public string Titular { get => titular; set => titular = value; }
+        public double Cantidad { get => cantidad; set => cantidad = value; }
+
+        public override string ToString()
+        {
+            return titular.ToString() + ": " + cantidad.ToString();
+        }
+        public void Ingresar(double dinero)
+        {
+            if(dinero >= 0)
+            {
+                cantidad += dinero;
+            }
+        }
+
+        public void Retirar(double dinero)
+        {
+            cantidad -= dinero;
+
+            if(cantidad < 0)
+            {
+                cantidad = 0;
+            }
+        }
+    }
+}
---
+++ UD10/UD10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace UD10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta("Eric Pastor", 1000);
+
+            string opcion = "";
+            string opcion2 = "";
+            
+
+            Console.WriteLine("Escoge una opcion entre el 1 y el 9");
+
+            opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine(cuenta1.ToString());
+                    Console.WriteLine("Desea ingresar o retirar dinero? (1 ingresar 2 retirar)");
+                    opcion2 = Console.ReadLine();
+
+                    if(opcion2 == "1")
+                    {
+                        Console.WriteLine("Cantidad a ingresar?");
+                        cuenta1.Ingresar(Convert.ToDouble(Console.ReadLine()));
+                    }
+                    else if (opcion2 == "2")
+                    {
+                        Console.WriteLine("Cantidad a retirar?");
+                        cuenta1.Retirar(Convert.ToDouble(Console.ReadLine()));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Opcion incorrecta");
+                    }
+                    Console.WriteLine(cuenta1.ToString());
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
